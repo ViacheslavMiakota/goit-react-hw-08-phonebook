2fd88c5fa9352568f58baad75b01abc273df5fa7
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2023-01-23 23:45:09 +0200

    addet corect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
 import { Provider } from 'react-redux';
@@ -6,9 +7,11 @@ import { store, persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
+  // {/* </React.StrictMode> */}
 );
