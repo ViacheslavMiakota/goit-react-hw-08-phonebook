1bbb6c43a2ef0e83dbebb89d1114e0460e198f12
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2023-01-24 15:48:13 +0200

    addet warning

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,32 @@
-import { Container, Title, ContactTitle } from 'components/App/App.styled';
+import { useSelector } from 'react-redux';
+import {
+  Container,
+  Title,
+  ContactTitle,
+  Warning,
+} from 'components/App/App.styled';
 import ContactForm from 'components/Form/Form';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
+import { getContacts } from 'redux/selectors';
 
 const App = () => {
+  const contacts = useSelector(getContacts);
   return (
     <Container>
       <Title>Phonebook</Title>
       <ContactForm />
       <ContactTitle>Contacts</ContactTitle>
-      <>
-        <Filter />
-        <ContactList />
-      </>
+      {contacts.length > 0 ? (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      ) : (
+        <Warning>
+          Your phone book is empty, your first contact has been added
+        </Warning>
+      )}
     </Container>
   );
 };
