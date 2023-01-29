584fb1471759d7f78b21b88bbcb87f887273b239
ViacheslavMiakota <104317136+ViacheslavMiakota@users.noreply.github.com>
2023-01-29 11:00:10 +0000

    Deploying to gh-pages from @ ViacheslavMiakota/goit-react-hw-08-phonebook@878c476c3fe2bfbd96a45b23130a86c1e88e3be9 🚀

---
+++ static/js/415.d9dc318f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Here will be your phone book. Please register."})})}}}]);
+//# sourceMappingURL=415.d9dc318f.chunk.js.map
